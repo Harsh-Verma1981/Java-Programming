c5bac3d1d4eb5fb973a79e942bae47b263f59248
Harsh Verma <harshv2208@gmail.com>
2026-01-30 10:49:44 +0530

    Implement binary exponentiation method in Master class

---
+++ Master.java
@@ -0,0 +1,18 @@
+public class Master{
+  public static int BinaryExpo(int a, intb){
+    int ans = 1;
+    while(b > 0){
+      if(b % 2 == 1){
+            ans = ans * a;
+      }
+      a = a * a;
+      b = b / 2;
+      
+    }
+    return ans;
+  }
+  public static void main(String[] argh){
+    int result = BinaryExpo(5, 10);
+    System.out.println(result);
+  }
+}
