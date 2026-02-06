a41054adb9e98bbbfe1644dff5fa351a2ba419c3
Harsh-Verma1981 <harshv2208@gmail.com>
2026-02-06 16:04:39 +0530

    new code

--- ResursiveSubset.java
+++ ResursiveSubset.java
@@ -1,14 +1,46 @@
 import java.util.*;
 
 public class ResursiveSubset {
-    public static void subset(){
-        
+    public static void subsequence(String s, int index, String current) {
+
+        // BASE CASE: we have processed all characters
+        if (index == s.length()) {
+            // if(!current.equals("")){
+            //     System.out.println(current);// condition 1
+            // }
+
+            // if(current.length() == 2){
+            //     System.out.println(current); // condition 2
+            // }
+
+            if(!current.contains("b")){
+                System.out.println(current); // condition 3
+            }
+
+            return;
+        }
+
+        /* 
+            this is going to run in the 2 phases 
+        firstly, run the choice 1 upto the base case (appending in the current str that's why we'll be seeing abc ab ac )
+        secondly, run the choice 2 upto the base case 
+        */
+
+       // CHOICE 2: do not take the character
+       subsequence(s, index + 1, current);// decline 
+       
+       // CHOICE 1: take the character
+        subsequence(s, index + 1, current + s.charAt(index));// accept
     }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        
+        /*
 
-        /* Basic Math operation revision */
+        int n = sc.nextInt();
+        
+        // Basic Math operation revision 
         double result = (double) n;
         // System.out.println(Math.pow(result, 2));// squre of n 
 
@@ -17,6 +49,14 @@ public class ResursiveSubset {
 
         int sqr = (int) Math.sqrt(result);
         System.out.println(sqr);
+        
+        */
+
+        String s = "abc";
+        String current = "";
+        int index = 0;
+
+        subsequence(s, index, current);
         
         sc.close();
     }
