bd855c3d85c6e1f706c5950ec494e98cbd4ffd7b
Harsh-Verma1981 <harshv2208@gmail.com>
2026-02-20 15:30:21 +0530

    updated file

--- BinarySearch.java
+++ BinarySearch.java
@@ -2,20 +2,29 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class BinarySearch {
-    public static int BinarySearch(int arr[], int k){
+    public static int BinarySearchforRotatedArray(int arr[], int k){
         int s = 0;
         int e = arr.length - 1;
-
+        // 6,7,8,9,1,2,3,4,5      k = 7;
         while(s <= e){
             int mid = s + (e - s) / 2;
 
             if(arr[mid] == k) return mid;
 
-            else if(arr[mid] < k){
-                s = mid - 1;
+            else if(arr[mid] >= arr[0] && k < arr[0]){
+                s = mid + 1;
             }
+
+            else if(arr[mid] < arr[0] && k >= arr[0]){
+                e = mid - 1;
+            }
+
+            else if(arr[mid] < k){
+                s = mid + 1;
+            }
+
             else{
-                e = mid + 1;
+                e = mid - 1;
             }
         }
 
@@ -30,8 +39,15 @@ public class BinarySearch {
 
         // System.out.println((BinarySearch(arr, k)));
 
+        // not going to work if elements of array gets rotated from k index .. 
         int result = Arrays.binarySearch(arr, k);
-        System.out.println(result);
+        // System.out.println(result);
+
+        int nums[] = {6,7,8,9,1,2,3,4,5};
+        int target = 7;// Thala for a reason
+
+        int index = BinarySearchforRotatedArray(nums, target);
+        System.out.println(index);// at index 1 
         
         sc.close();
     }
