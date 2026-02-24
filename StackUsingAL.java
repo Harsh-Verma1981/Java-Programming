f205c3645618d82fb36b8bcc4363fbb4d0275701
Harsh-Verma1981 <harshv2208@gmail.com>
2026-02-24 12:48:51 +0530

    stack files using LL and AL

---
+++ StackUsingAL.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StackUsingAL {
+    static class Stack{
+        static ArrayList<Integer> list = new ArrayList<>();
+
+        public static boolean isEmpty(){
+            return list.size() == 0;
+        }
+
+        public void push(int data){
+            list.add(data);
+        }
+
+        public static int pop(){
+            if(isEmpty()) return -1;
+
+            int top = list.get(list.size() - 1);
+
+            list.remove(list.size() - 1);
+
+            return top;
+        }
+
+        public static int peek(){
+            if(isEmpty()) return -1;
+
+            return list.get(list.size() - 1);
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        Stack s = new Stack();
+
+        s.push(12);
+        s.push(1);
+        s.push(3);
+        s.push(22);
+
+        while(!s.isEmpty()){
+            System.out.print(s.peek() + " ");
+            s.pop();
+        }System.out.println();
+
+        for(int i = 0;i < 3;i++){
+            System.out.print("Enter the element: ");
+            int data = sc.nextInt();
+            s.push(data);
+        }
+
+        System.out.println("Peek Element: " + s.peek());
+
+        sc.close();
+    }    
+}
