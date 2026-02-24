e7a17a232f0c2d0125bd43340e90918fb2d7146c
Harsh-Verma1981 <harshv2208@gmail.com>
2026-02-24 13:27:05 +0530

    Stack Questions basic

---
+++ StackQues.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class StackQues {
+    public static void pushAtBottom(int data, Stack<Integer> st){
+        // using recursion
+
+        // base case
+        if(st.empty()){
+            st.push(data);
+            return;
+        }
+
+        int top = st.pop();
+        pushAtBottom(data, st);
+        st.push(top);
+
+        /*
+      P  |3 pop      pushing 3 again       <-| P
+      O  |2 pop      pushing 2 again         | U
+      P  |1 pop      pushing 1 again         | S
+         |->Empty()  4 push then             | H
+        */
+
+    }
+
+    public static void reverseStack(Stack<Integer> st){
+        if(st.empty()) return;
+
+        int top = st.pop();
+
+        reverseStack(st);
+        pushAtBottom(top, st);
+
+
+    }
+    public static void main(String[] args){
+        Stack<Integer> st = new Stack<>();
+
+        st.push(1);
+        st.push(2);
+        st.push(3);
+
+        // pushAtBottom(4, st);// pushing 4 at bottom 
+        reverseStack(st);
+
+        while(!st.empty()) {
+            System.out.print("|" + st.peek() + "|");
+            st.pop();
+        }
+    }
+}
