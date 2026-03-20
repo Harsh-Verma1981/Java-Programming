8141e8559f0c88a79e4b6c83e7fcfe1bafdfe931
Harsh-Verma1981 <harshv2208@gmail.com>
2026-03-20 09:48:42 +0530

    Base case -ve pow

--- BinaryExpo.java
+++ BinaryExpo.java
@@ -2,8 +2,12 @@ import java.util.Scanner;
 
 public class BinaryExpo{
   // to find the pow of a to the power b
-    public static int BinaryExponential(int a, int b){
-      int ans = 1;
+    public static double BinaryExponential(double a, int b){
+      if(b < 0){ //  base case for negative powers .. 
+            b = -b;
+            a = 1 / a;
+      }
+      double ans = 1;
       while(b > 0){
         if(b % 2 == 1){
           ans = ans * a;
@@ -16,8 +20,8 @@ public class BinaryExpo{
       return ans;
     }
     public static void main(String[] argh){
-      int result = BinaryExponential(5, 5);
-      System.out.println(result);// op is 3125
+      double result = BinaryExponential(2.00, -2);
+      System.out.printf("%.5f" ,result);// op is 0.25000
       
     }
 }
