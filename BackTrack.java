8c44d39e758751895cd1bde083cbd37b758a7070
Harsh-Verma1981 <harshv2208@gmail.com>
2026-05-07 23:43:09 +0530

    func way of recursion

--- BackTrack.java
+++ BackTrack.java
@@ -36,6 +36,38 @@ public class BackTrack {
         return n + FuncSum(n - 1);
     }
 
+    // Array reverse
+    public static void ReverseArr(int low, int high, int []arr){
+        if(low >= high){
+            for(int i = 0;i < arr.length;i++){
+                System.out.print(arr[i] + " ");
+            }System.out.println();
+            return;
+        }
+        // swapping logic 
+        int temp = arr[low];
+        arr[low] = arr[high];
+        arr[high] = temp;
+        ReverseArr(low + 1, high - 1, arr);
+
+    }
+
+    public static void SinglePtrReversal(int i, int[] arr){
+        int n = arr.length;
+        if(i >= (n/2)) {
+            for(int j = 0;j < n;j++){
+                System.out.print(arr[j] + " ");
+            }System.out.println();
+            return;
+        }
+
+        // swapping logic
+        int first = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = first;
+
+        SinglePtrReversal(first + 1, arr);
+    }
     public static void main(String[] argh){
         Scanner sc = new Scanner(System.in);
 
@@ -50,5 +82,11 @@ public class BackTrack {
         ParaSum(n, sum);
 
         System.out.println("Functional way for sum " + FuncSum(n));
+        int []arr = {1,2,3,4,5};
+        int i = 0; int j = arr.length - 1;
+
+        // ReverseArr(i, j, arr);
+        SinglePtrReversal(i, arr);
+
     }    
 }
