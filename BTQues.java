fa287d0371cf17c2d90356ad612a81d0b50634d3
Harsh-Verma1981 <harshv2208@gmail.com>
2026-05-26 15:49:17 +0530

    Balanced Tree Question

--- BTQues.java
+++ BTQues.java
@@ -12,10 +12,25 @@ class Node{
 }
 
 public class BTQues {
+
     public static int MaxDepth(Node root){
         if(root == null) return 0;
+        
+        int leftT = MaxDepth(root.left);
+        if(leftT == -1) return -1;
 
-        return 1 + Math.max(MaxDepth(root.left), MaxDepth(root.right));
+        int rightT =  MaxDepth(root.right);
+        if(rightT == -1) return -1;
+
+        if(Math.abs(rightT - leftT) > 1) return -1;
+
+        return 1 + Math.max(leftT, rightT);
+    }
+
+    // A balance BT is a tree in which the depth of the two subtrees of every node never differs by more than one.
+    public static boolean isBalancedTree(Node root){
+        // Key point: left -right > 1 for balanced BT
+        return MaxDepth(root) != -1; 
     }
     public static void main(String[] args){
         Node root = new Node(1);
@@ -31,5 +46,7 @@ public class BTQues {
 
         int heightOfTree = MaxDepth(root);
         System.out.println(heightOfTree);
+
+        System.out.println(isBalancedTree(root));
     }
 }
