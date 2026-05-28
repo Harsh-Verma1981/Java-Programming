9c8d40f85907c2a18460b874924ebc185d7d3192
Harsh-Verma1981 <harshv2208@gmail.com>
2026-05-28 16:22:24 +0530

    bt new

--- BTQues.java
+++ BTQues.java
@@ -50,8 +50,22 @@ public class BTQues {
 
         return longpath;
     }
+    
+    public static boolean isSameTree(Node p, Node q){
+        if(p == null && q == null) return true;
+
+        if(p == null || q == null) return false;
+
+        if(p.val != q.val) return false;
+
+        return(
+            isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+        );
+    }
+
     public static void main(String[] args){
         Node root = new Node(1);
+        Node root2 = new Node(11);
 
         root.left = new Node(2);
         root.right = new Node(3);
@@ -69,6 +83,9 @@ public class BTQues {
 
         int diameter = diameterOfBT(root);
 
-        System.out.println(diameter);
+        System.out.println("Longest path is " + diameter);
+
+        boolean result = isSameTree(root, root);
+        System.out.println(result);
     }
 }
