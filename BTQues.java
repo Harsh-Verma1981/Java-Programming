a4fcb79a3e96eee988dce5b7a85a602bba51a534
Harsh-Verma1981 <harshv2208@gmail.com>
2026-05-29 15:37:44 +0530

    isSymmetric tree

--- BTQues.java
+++ BTQues.java
@@ -63,9 +63,21 @@ public class BTQues {
         );
     }
 
+    public static boolean isSymmetric(Node p, Node q){
+        if(p == null && q == null) return true;
+
+        if(p == null || q == null) return false;
+
+        if(p.val != q.val) return false;
+
+        return(
+            isSymmetric(p.left, q.right) && isSymmetric(p.right, q.left)
+        );
+    }
+
     public static void main(String[] args){
         Node root = new Node(1);
-        Node root2 = new Node(11);
+        Node root2 = new Node(1);
 
         root.left = new Node(2);
         root.right = new Node(3);
@@ -75,6 +87,15 @@ public class BTQues {
 
         root.right.left = new Node(6);
         root.right.right = new Node(7);
+
+        root2.left = new Node(2);
+        root2.right = new Node(2);
+
+        root2.left.left = new Node(4);
+        root2.left.right = new Node(4);
+
+        root2.right.left = new Node(6);
+        root2.right.right = new Node(6);
 
         int heightOfTree = MaxDepth(root);
         System.out.println(heightOfTree);
@@ -87,5 +108,8 @@ public class BTQues {
 
         boolean result = isSameTree(root, root);
         System.out.println(result);
+
+        boolean symetric = isSymmetric(root2.left, root2.right);// left subtree equals to right subtree
+        System.out.println(symetric);
     }
 }
