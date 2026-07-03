39bf853504797d3dd7e7b09e3674839ca752636d
Harsh-Verma1981 <harshv2208@gmail.com>
2026-07-03 14:09:11 +0530

    3/7/26

--- BTMaxSum.java
+++ BTMaxSum.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Node {
     int val;
     Node left; Node right;
@@ -10,8 +12,37 @@ class Node {
 
 public class BTMaxSum {
     public static int MaxSumLevel(Node root){
-        return 0;
+        if(root == null) return 0;
+
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+
+        int max_sum = Integer.MIN_VALUE;
+        int curr_lvl = 1; int max_lvl = 1;
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            int sum = 0;
+
+            for(int i = 0;i < size;i++){
+                Node node = q.poll();
+                sum += node.val;
+
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
+            }
+
+            if(sum > max_sum){
+                max_sum = sum;
+                max_lvl = curr_lvl;
+            }
+
+            curr_lvl++;
+        }
+
+        return max_lvl;
     }
+
     public static void main(String []args){
         Node root = new Node(1);
         root.left = new Node(2);
