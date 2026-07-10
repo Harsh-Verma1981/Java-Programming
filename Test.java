e6192de046cc776751c3bbe29886a26dd7ffa718
Harsh-Verma1981 <harshv2208@gmail.com>
2026-07-10 15:22:26 +0530

    2nd commit

--- Test.java
+++ Test.java
@@ -1,6 +1,46 @@
 import java.util.*;
 
 public class Test{
+    public static boolean isEqual(String s, String t){
+        Stack<Character> st1 = new Stack<>();
+        Stack<Character> st2 = new Stack<>();
+
+        for(int i = 0;i < s.length();i++){
+            char ch = s.charAt(i);
+
+            if(!st1.isEmpty() && ch == '?'){
+                st1.pop();
+            }
+            else {
+                st1.push(ch);
+            }
+        }
+
+        for(int i = 0;i < t.length();i++){
+            char ch = t.charAt(i);
+
+            if(!st2.isEmpty() && ch == '?'){
+                st2.pop();
+            } else{
+                st2.push(ch);
+            }
+        }
+
+        StringBuilder sb1 = new StringBuilder(); StringBuilder sb2 = new StringBuilder();
+        while(!st1.isEmpty()){
+            sb1.insert(0, st1.pop());
+        }
+
+        while(!st2.isEmpty()){
+            sb2.insert(0, st2.pop());
+        }
+
+        String a = sb1.toString();
+        String b = sb2.toString();
+
+        return a.equals(b);
+    }
+
     public static long CountOnes(long []arr, int k, int m){
         long count = 0;
         long sum = 0;
@@ -24,10 +64,10 @@ public class Test{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("Enter the no of test cases: ");
-        int test = sc.nextInt();
+        // System.out.print("Enter the no of test cases: ");
+        // int test = sc.nextInt();
 
-        while(test-- > 0){
+        /* while(test-- > 0){
             System.out.print("Enter the size: ");
             int n = sc.nextInt();// size of array
             
@@ -44,7 +84,14 @@ public class Test{
             }
 
             System.out.print(CountOnes(arr, k, m));
-        }
+        } */
+
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+
+        if(isEqual(s, t)){
+            System.out.println("YES");
+        } else System.out.println("NO");
         
         sc.close();
     }
